a997e7e74b3bfbabe034680223063f1d04ce4c60
shkelmark <maggie29204@gmail.com>
2017-10-07 18:13:03 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace GammaRadiation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument myDoc = new XmlDocument();
+            myDoc.Load("C:\\Users\\maggi\\Documents\\test\\GammaRadiation\\task2017_02.xml"); 
+           
+            
+        }
+    }
+}
